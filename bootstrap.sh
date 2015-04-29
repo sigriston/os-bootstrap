2bc381c6b9d03ab4e74baff1bf7e118ac4f3083b
Thiago Sigrist <sigrist@gmail.com>
2015-04-29 13:48:15 -0300

    finish bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install homebrew bundle & bundle up!
 brew tap homebrew/bundle
+brew bundle
