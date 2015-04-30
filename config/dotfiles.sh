5bf5eaa4732badb6b9042df120729411d8857b66
Thiago Sigrist <sigrist@gmail.com>
2015-04-30 08:50:06 -0300

    link all dotfiles from dotfiles.sh

--- config/dotfiles.sh
+++ config/dotfiles.sh
@@ -8,3 +8,12 @@ mkdir -p ~/src
 pushd ~/src
 git clone https://github.com/sigriston/dotfiles
 popd
+
+# link dotfiles
+# link .gitconfig
+ln -s ~/src/dotfiles/git/gitconfig ~/.gitconfig
+# link .vimrc, .gvimrc
+ln -s ~/src/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/src/dotfiles/vim/gvimrc ~/.gvimrc
+# link .spacemacs
+ln -s ~/src/dotfiles/spacemacs/spacemacs ~/.spacemacs
