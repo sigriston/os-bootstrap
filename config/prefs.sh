81c44fbe3961e70a768c74c88513fee1148c1c35
Thiago Sigrist <sigrist@gmail.com>
2015-04-30 07:52:50 -0700

    fix homedir refs in prefs.sh

--- config/prefs.sh
+++ config/prefs.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 
+PREFSDIR="${HOME}/Library/Preferences"
+
 # iTerm2
 ITERM2_PREF="com.googlecode.iterm2"
 ITERM2_PREFFILE="${ITERM2_PREF}.plist"
 ITERM2_PREFFILE_SRC="./plists/${ITERM2_PREFFILE}"
-ITERM2_PREFFILE_TGT="~/Library/Preferences/${ITERM2_PREFFILE}"
+ITERM2_PREFFILE_TGT="${PREFSDIR}/${ITERM2_PREFFILE}"
 
 if [[ -s "${ITERM2_PREFFILE_TGT}" ]]; then
   defaults delete "${ITERM2_PREF}"
@@ -13,4 +15,4 @@ if [[ -s "${ITERM2_PREFFILE_TGT}" ]]; then
 fi
 
 cp "${ITERM2_PREFFILE_SRC}" "${ITERM2_PREFFILE_TGT}"
-defaults read "${ITERM2_PREF}"
+defaults read "${ITERM2_PREF}" > /dev/null
