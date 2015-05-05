5053663d8e6a252dbd379509fd7b2dff93f8c0fd
Thiago Sigrist <sigrist@gmail.com>
2015-05-05 10:17:15 -0300

    sudo only once for truly unattended setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,18 @@
 #!/bin/sh -e
 
 
+######################################################################
+## - sudo only once
+##   code taken from .osx by Mathias Bynens (https://mths.be/osx)
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+######################################################################
+
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
