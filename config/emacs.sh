001d9b56179546792c46d76895ad3c74c00e57fe
Thiago Sigrist <sigrist@gmail.com>
2015-07-06 00:53:45 -0300

    cleanup: remove config dir

--- config/emacs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-
-# link Emacs.app under Applications
-ln -s $(brew --prefix emacs-mac)/Emacs.app ~/Applications
-
-# install spacemacs
-rm -rf ~/.emacs.d
-git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d
