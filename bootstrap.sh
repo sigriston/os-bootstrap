5c642e31a324277c8e2e40a1cab5f94adfda8287
Thiago Sigrist <sigrist@gmail.com>
2016-07-15 19:03:33 -0300

    use latest Python 2 from homebrew for everything

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,15 @@
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install Python 2 from homebrew
+brew install python
+brew linkapps python
+
 # Install homebrew-cask
 brew tap caskroom/cask
 
 # Install ansible
-brew install ansible
+pip install ansible
 
 # Now run ansible playbook and be happy!
 ansible-playbook -i hosts --ask-become-pass playbook.yml
